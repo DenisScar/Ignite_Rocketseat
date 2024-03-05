e79493795cc213db67768f552de08f731a8a4adf
Denis S. Cardoso <48637793+DenisScar@users.noreply.github.com>
2024-03-05 03:12:20 +0000

    create header component

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Post} from "./Post";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./styles.css";
 
+export function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Post
+        author="Denis Cardoso"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias suscipit neque architecto, voluptatibus, id quam laboriosam ullam velit omnis sint quos nesciunt commodi porro! Consequatur excepturi dicta impedit adipisci doloremque!"
+      />
+      <Post
+        author="Gisele Lopes"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ipsam pariatur quo ipsa, eius nobis laudantium suscipit provident repellendus similique totam officia magni sit recusandae architecto. Pariatur natus distinctio culpa."
+      />
+    </div>
   )
 }
-
-export default App
